81593932328d136d535fc4deb6a136d5e638f4d1
Chris Hunter <orkem2@gmail.com>
2017-09-22 14:45:38 +0100

    adds basic integration test for add, multiply, divide and subtract

--- Unit and Integration Tasks B/js_calculator_start_point/tests/integration/tests.js
+++ Unit and Integration Tasks B/js_calculator_start_point/tests/integration/tests.js
@@ -11,9 +11,49 @@ describe('calculator functionality', function() {
 
   // write integration tests here in the form of "it should do something..."
   it('should have working number buttons', function(){
-    running_total = element(by.css('#running_total'))
+    var running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+    expect(running_total.getAttribute('value')).to.eventually.equal('2');
+  })
+
+  it('should have working arithmetic buttons including plus', function(){
+    var running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    add = element(by.css('#operator_add'));
+    add.click();
+    element(by.css('#number3')).click();
+    add.click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5');
+  })
+
+  it('should have working arithmetic buttons including minus', function(){
+    var running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    subtract = element(by.css('#operator_subtract'));
+    subtract.click();
+    element(by.css('#number2')).click();
+    subtract.click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1');
+  })
+
+  it('should have working arithmetic buttons including divide', function(){
+    var running_total = element(by.css('#running_total'))
+    element(by.css('#number8')).click();
+    divide = element(by.css('#operator_divide'));
+    divide.click();
+    element(by.css('#number2')).click();
+    divide.click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4');
+  })
+
+  it('should have working arithmetic buttons including multiply', function(){
+    var running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    multiply = element(by.css('#operator_multiply'));
+    multiply.click();
+    element(by.css('#number3')).click();
+    multiply.click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('27');
   })
 
 });
